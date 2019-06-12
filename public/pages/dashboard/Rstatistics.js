83a2a11f63e405e5cd7d151071a15c09ac62f853
MikolajKaminski <mikolaj.kaminski@student.stenden.com>
2019-06-13 01:03:38 +0200

    Removed useless imports

--- public/pages/dashboard/Rstatistics.js
+++ public/pages/dashboard/Rstatistics.js
@@ -95,8 +95,8 @@ async function populateComments(){
           if(comment.data().active == true) {
             // Create the box layout
             var text = "<span class=\"small pull-right\" id=\"commentdate" + i + 
-                       "\"></span><br /><span class=\"small info-box-number pull-right\">" +
-                       comment.data().user + "</span>" + 
+                       "\"></span><br /><span class=\"pull-right\"><b>" +
+                       comment.data().user + "</b></span>" + 
                        comment.data().comment;
             // Add text to document
             document.getElementById("commentbox" + i).innerHTML = text;
@@ -200,3 +200,86 @@ function seeAttraction(id, description, latitude, longitude, direction) {
   sessionStorage.setItem('direction',   direction);
   window.location.assign('../../pages/locations/locations-add.html');
 }
+
+//Admin LTE Stuff
+$(function () {
+
+  'use strict';
+
+  // bootstrap WYSIHTML5 - text editor
+  $('.textarea').wysihtml5();
+
+  $('.daterange').daterangepicker({
+    ranges   : {
+      'Today'       : [moment(), moment()],
+      'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+      'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+      'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+      'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    },
+    startDate: moment().subtract(29, 'days'),
+    endDate  : moment()
+  }, function (start, end) {
+    window.alert('You chose: ' + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+  });
+
+  /* jQueryKnob */
+  $('.knob').knob();
+
+  // jvectormap data
+  var visitorsData = {
+    US: 398, // USA
+    SA: 400, // Saudi Arabia
+    CA: 1000, // Canada
+    DE: 500, // Germany
+    FR: 760, // France
+    CN: 300, // China
+    AU: 700, // Australia
+    BR: 600, // Brazil
+    IN: 800, // India
+    GB: 320, // Great Britain
+    RU: 3000 // Russia
+  };
+  // World map by jvectormap
+  $('#world-map').vectorMap({
+    map              : 'world_mill_en',
+    backgroundColor  : 'transparent',
+    regionStyle      : {
+      initial: {
+        fill            : '#e4e4e4',
+        'fill-opacity'  : 1,
+        stroke          : 'none',
+        'stroke-width'  : 0,
+        'stroke-opacity': 1
+      }
+    },
+    series           : {
+      regions: [
+        {
+          values           : visitorsData,
+          scale            : ['#92c1dc', '#ebf4f9'],
+          normalizeFunction: 'polynomial'
+        }
+      ]
+    },
+    onRegionLabelShow: function (e, el, code) {
+      if (typeof visitorsData[code] != 'undefined')
+        el.html(el.html() + ': ' + visitorsData[code] + ' new visitors');
+    }
+  });
+
+  // The Calender
+  $('#calendar').datepicker();
+
+  /* The todo list plugin */
+  $('.todo-list').todoList({
+    onCheck  : function () {
+      window.console.log($(this), 'The element has been checked');
+    },
+    onUnCheck: function () {
+      window.console.log($(this), 'The element has been unchecked');
+    }
+  });
+
+});
