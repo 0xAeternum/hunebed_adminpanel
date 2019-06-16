d53027ef45d09cbaef70d4709875bcfa2e356cb2
MartinTonev <martin.e.tonev@gmail.com>
2019-06-16 19:36:23 +0200

    Anyone can sign in

---
+++ public/pages/login.js
@@ -0,0 +1,33 @@
+var config = {
+    apiKey: "AIzaSyC37ynDc3SyuxzwDcQLWs3luTbfz-MLSfw",
+    authDomain: "fir-test-c91f4.firebaseapp.com",
+    databaseURL: "https://fir-test-c91f4.firebaseio.com",
+    projectId: "fir-test-c91f4",
+    storageBucket: "fir-test-c91f4.appspot.com",
+    messagingSenderId: "231032087489",
+    appId: "1:231032087489:web:37267e6ec937a3e6"
+  };
+  firebase.initializeApp(config);
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+
+function login(){
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user.email)
+
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+}
